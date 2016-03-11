ffce421f457b4881d60d7d9da997f21b2c3e1b4a
arktisklada <mail@enorganik.com>
2016-03-11 16:43:59 -0600

    Removing unused timeout code

--- src/foreman/timeout.cr
+++ src/foreman/timeout.cr
@@ -1,37 +1,8 @@
 module Timeout
-  def self.timeout(seconds, error_handler, &block)
+  def self.timeout(seconds, timeout_handler, &block)
     return block.call if seconds == nil || seconds == 0
 
-    # error = TimeoutError.new("execution expired")
-
-    timeout_thread = delay(seconds) { error_handler.call }
+    timeout_thread = delay(seconds) { timeout_handler.call }
     block.call
-
-    # while !timeout_reached
-    # end
-
-    # execution_thread
-    # channel = Channel(Bool).new
-    # execution_thread = spawn do
-    #   channel.send true
-    # end
-
-    # timer_thread = spawn do
-    #   sleep seconds
-    #   channel.send false
-    # end
-
-    # channel_response = channel.receive
-    # channel.close
-
-    # if channel_response == false
-
-    # end
-  end
-
-  class TimeoutError < Exception
-    def initialize(message = "Timeout reached")
-      super(message)
-    end
   end
 end
